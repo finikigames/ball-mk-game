19dfb30b25ff76ca84c3b744c738d791070596c4
Anton Sukharev <sanjaplushka@gmail.com>
2022-11-18 18:09:14 +0500

    Inverse movement

---
+++ Assets/Scripts/BallMovementInverse.cs
@@ -0,0 +1,17 @@
+﻿using Services;
+using UnityEngine;
+
+namespace DefaultNamespace {
+    public class BallMovementInverse : MonoBehaviour {
+        public void Update() {
+            var input = InputService.Instance.Input.Value;
+            var inversedInput = new Vector2(-input.x, -input.y);
+            InputService.Instance.Input.Value = inversedInput;
+            InputService.Instance.InputVector = inversedInput;
+
+            var inputClamped = InputService.Instance.InputVectorClamped;
+            var inputClampedInverse = new Vector2(-inputClamped.x, -inputClamped.y);
+            InputService.Instance.InputVectorClamped = inputClampedInverse;
+        }
+    }
+}
