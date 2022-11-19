8b821a951c83823a3f0c0dd58d274d597ced6f42
Victor <wionvictor@gmail.com>
2022-11-20 00:32:37 +0500

    Simple teleport to point and add player tag only on sphere object

---
+++ Assets/Scripts/TeleportToPoint.cs
@@ -0,0 +1,16 @@
+using System;
+using Extensions;
+using UnityEngine;
+
+public class TeleportToPoint : MonoBehaviour {
+    public Vector3 EndPoint;
+    private const string _playerlayerName = "Player";
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer != LayerMask.NameToLayer(_playerlayerName)) return;
+
+        var rigidBody = other.gameObject.AddComponentLazy<Rigidbody>();
+        other.transform.position = EndPoint;
+        rigidBody.velocity = Vector3.zero;
+    }
+}
